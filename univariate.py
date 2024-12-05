afa77cba001700403d8693c2187c24565d492844
Jelle Westra <jelwestra@gmail.com>
2024-12-05 09:04:05 +0100

    gpu support

--- univariate.py
+++ univariate.py
@@ -12,6 +12,8 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 
 import argparse
+import sys
+import platform
 from glob import glob
 from tqdm import tqdm
 from typing import Tuple
@@ -19,11 +21,11 @@ from typing import Tuple
 import data
 
 class UniDataset(Dataset):
-    def __init__(self, df: pd.DataFrame, column: str='NL01485', in_days=5, out_days=2) -> None : 
-        self.x = torch.tensor(df[column].values, dtype=torch.float32)
+    def __init__(self, df: pd.DataFrame, column: str='NL01485', in_days=4, out_days=14, device='cpu') -> None : 
+        self.x = torch.tensor(df[column].values, dtype=torch.float32, device=device)
         # self.t = torch.tensor(df['Time.Continuous'].values, dtype=torch.float32)
         # TODO : add more time features not only hour
-        self.t = torch.tensor(df['Hour'].values, dtype=torch.float32)/24
+        self.t = torch.tensor(df['Hour'].values, dtype=torch.float32, device=device)/24
         
         (self.in_samples, self.out_samples) = (24*in_days, 24*out_days)
         self.n = len(self.x) - 2*(self.in_samples + self.out_samples)
@@ -44,7 +46,8 @@ class UniDataset(Dataset):
 def train(args, model, device, train_loader, optimizer, epoch) :
     model.train()
 
-    for batch_idx, (data, data_time, data_mask, target, target_time) in enumerate(train_loader) :
+    loss_avg = 0
+    for batch_idx, (data, data_time, data_mask, target, target_time) in tqdm(enumerate(train_loader), leave=False, total=args.batches_per_epoch) :
         optimizer.zero_grad()
 
         outputs = model(
@@ -58,13 +61,16 @@ def train(args, model, device, train_loader, optimizer, epoch) :
         loss.backward()
         optimizer.step()
 
+        loss_avg += (loss.item()/args.batches_per_epoch)
         if (batch_idx >= args.batches_per_epoch) : break
+    
+    return loss_avg
 
 def eval(args, model, val_loader, epoch):
     model.eval()
     with torch.no_grad():
         rmse = 0
-        for batch_idx, (data, data_time, data_mask, target, target_time) in enumerate(val_loader) :
+        for batch_idx, (data, data_time, data_mask, target, target_time) in tqdm(enumerate(val_loader), leave=False, total=args.batches_per_val) :
             outputs = model.generate(
                 past_values=data,
                 past_time_features=data_time,
@@ -76,30 +82,35 @@ def eval(args, model, val_loader, epoch):
             rmse += (target - pred).square().mean().sqrt() / args.batches_per_val
             if (batch_idx >= args.batches_per_val) : break
 
-        print(f'[{epoch+1:02d}/{args.epochs:02d}] {rmse.item()}')
+    return rmse.item()
 
 
 def parse_args() -> argparse.Namespace :
     parser = argparse.ArgumentParser(description='Univariate Autoformer')
-    parser.add_argument('--batch-size', type=int, default=128)
+    parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--batches-per-epoch', type=int, default=100)
     parser.add_argument('--batches-per-val', type=int, default=8)
     parser.add_argument('--epochs', type=int, default=10)
 
     parser.add_argument('--in-days', type=int, default=4)
-    parser.add_argument('--out-days', type=int, default=1)
+    parser.add_argument('--out-days', type=int, default=14)
 
     return parser.parse_args()
 
 def main(args) :
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('cpu')
+    
+    print(sys.version)
+    print(platform.node(), platform.platform())
+    print(device)
 
     train_df = data.load(years=['2017', '2018', '2019', '2020', '2021'])
-    train_dataset = UniDataset(train_df, in_days=args.in_days, out_days=args.out_days)
+    train_dataset = UniDataset(train_df, in_days=args.in_days, out_days=args.out_days, device=device)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
 
     val_df = data.load(years=['2022'])
-    val_dataset = UniDataset(val_df, in_days=args.in_days, out_days=args.out_days)
+    val_dataset = UniDataset(val_df, in_days=args.in_days, out_days=args.out_days, device=device)
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
 
     # TODO : setup the config
@@ -109,13 +120,14 @@ def main(args) :
         num_time_features=1,
         d_model=16,
     )
-    model = AutoformerForPrediction(config)
+    model = AutoformerForPrediction(config).to(device)
     
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
     for epoch in range(args.epochs):
-        train(args, model, device, train_loader, optimizer, epoch)
-        eval(args, model, val_loader, epoch)
+        loss_avg = train(args, model, device, train_loader, optimizer, epoch)
+        val_rmse = eval(args, model, val_loader, epoch)
+        print(f'[{epoch+1:02d}/{args.epochs:02d}] {loss_avg:.3f} {val_rmse:.3f}')
 
 
     torch.save(model.state_dict(), 'univariate_model.pth')
