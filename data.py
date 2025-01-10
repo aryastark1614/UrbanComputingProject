1246103ea0ede1d3843b5b97b29d64bd13957553
Jelle Westra <jelwestra@gmail.com>
2025-01-10 10:15:23 +0100

    regular operation mode

--- data.py
+++ data.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
+import json
 import requests
 import os
 from itertools import product
 from tqdm import tqdm
 
-from typing import List
+from typing import List, Set
 
 
 def download_file(url: str, path: str) -> None :
-    '''downloading the file for the given url and saving it on out'''
+    # downloading the file for the given url and saving it on out
 
     response = requests.get(url)
     if (response.status_code == 200):
@@ -18,36 +19,58 @@ def download_file(url: str, path: str) -> None :
     else:
         raise Exception(f'failed to download {url} [{response.status_code}]')
     
+
+def _load_indicator(year: str='2017', indicator: str='NOx', stations: Set[str]=None) -> pd.DataFrame :
+    if (indicator == 'NO2') : 
+        df = _load_NO2(year)    
+    else:
+        df = pd.read_csv(f'./data/{indicator}/{year}.csv', sep=';', skiprows=5, encoding='latin-1')
+
+        df.rename(columns={'begindatumtijd': 'time', 'meetlocatie_id': 'id', 'waarde': 'value'}, inplace=True)
+        df['time'] = pd.to_datetime(df['time'], utc=True)
+        df.drop(['meetreeks_id', 'bron_id', 'accreditatienummer', 'component', 'matrix', 'meetopstelling_id', 'meetduur', 'eenheid', 'einddatumtijd', 'opm_code'], axis=1, inplace=True)
+
+        df = df.pivot(index='time', columns='id', values='value')
+
+    df.drop([col for col in df.columns if col not in stations], axis=1, inplace=True)
+    df.rename(lambda col : f'{col.strip()}-{indicator}', axis=1, inplace=True)
+
+    return df
+
+def _load_NO2(year: str='2017') -> pd.DataFrame :
+    df = pd.read_csv(f'./data/NO2/{year}.csv', sep=';', skiprows=9, encoding='latin-1')
+
+    # removing spacing errors in the column names
+    df.columns = pd.Index([col.strip() for col in df.columns])
+
+    df.rename(columns={'Begindatumtijd': 'time'}, inplace=True)
+    df['time'] = pd.to_datetime(df['time'], utc=True)
+    df.set_index('time', inplace=True)
+
+    df.drop([col for col in df.columns if not(col.startswith('NL'))], axis=1, inplace=True)
+
+    return df
+
 def prep(
     years: List[str]=['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
     indicators: List[str]=['NO2']
 ) -> pd.DataFrame:
-    '''prepping the downloaded csv's'''
+    # prepping the downloaded csv's
+    with open('./data/stations.json', 'r') as handle : stations = json.load(handle)
 
     df = pd.concat([
-        pd.read_csv(f'./data/{indicator}/{year}.csv', sep=';', skiprows=9, encoding='latin-1') 
-        for (indicator, year) in product(indicators, years)
-    ])
-    # removing spacing errors in the column names
-    df.columns = pd.Index([col.strip() for col in df.columns])
+        pd.concat([_load_indicator(year, indicator, stations) for year in tqdm(years, desc=indicator)])
+        for indicator in indicators
+    ], axis=1)
 
-    df.rename(columns={'Begindatumtijd': 'Time'}, inplace=True)
-    df['Time'] = pd.to_datetime(df['Time'])
-    df.set_index('Time', inplace=True)
-
-    df.drop([col for col in df.columns if not(col.startswith('NL'))], axis=1, inplace=True)
-    
+    df.fillna(0, inplace=True)
+   
     df['Hour'] = df.index.hour
     df['DayOfYear'] = df.index.dayofyear
     df['Day'] = df.index.day
     df['Month'] = df.index.month
     df['Year'] = df.index.month
     df['Time.Continuous'] = df.index.astype('int64') / 1e10 / 60
-
-    # TODO : how are we going to handle the NaN's?
-    #        maybe it's better to just removing any instances containing them while batching the data?
-    df.ffill(inplace=True)
-    df.bfill(inplace=True)
 
     return df
     
---
+++ regular-eval.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+from transformers import AutoformerForPrediction, AutoformerConfig
+import torch
+from regular import MultiDataset
+from data import load
+
+from tqdm import tqdm
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--station', type=str, default=None)
+args = parser.parse_args()
+
+config = AutoformerConfig.from_json_file('regular_config.json')
+model = AutoformerForPrediction(config)
+model.load_state_dict(torch.load('regular_model.pth', weights_only=True))
+model.eval();
+
+train_df = load(years=['2017', '2018', '2019', '2020'], indicators=['NOx', 'NO', 'NO2'])
+train_dataset = MultiDataset(train_df, stations=[args.station] if args.station is not None else None, in_days=4, out_days=14)
+
+torch.manual_seed(3141)
+val_df = load(years=['2021', '2022'], indicators=['NOx', 'NO', 'NO2'])
+val_dataset = MultiDataset(val_df, stations=[args.station] if args.station is not None else None, in_days=4, out_days=14, m=train_dataset.m, s=train_dataset.s)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=8, shuffle=True)
+
+losses = []
+pbar = tqdm(total=10, leave=False)
+for batch_idx, (data, data_time, data_mask, static_real_features, target, target_time) in enumerate(val_loader) :
+    outputs = model.generate(
+        past_values=data,
+        past_time_features=data_time,
+        past_observed_mask=data_mask,
+        future_time_features=target_time
+    )
+
+    pred = outputs.sequences.mean(dim=1)
+    
+    if (len(pred.shape) < 3) : 
+        data = data.view(*data.shape, 1)
+        target = target.view(*target.shape, 1)
+        pred = pred.view(*pred.shape, 1)
+    
+    data = (data * train_dataset.s + train_dataset.m)
+    target = (target * train_dataset.s + train_dataset.m)
+    pred = (pred * train_dataset.s + train_dataset.m)
+
+    # only evaluate the forecast on NOx
+    if (pred.shape[-1] > 39) : 
+        data = data[:,:,:39]
+        target = target[:,:,:39]
+        pred = pred[:,:,:39]
+
+    losses.append((target - pred).square().mean().sqrt().item())
+
+    pbar.update()
+    if batch_idx >= (10-1) : break
+
+print(f'{args.station}, {np.mean(losses)}, {np.std(losses)}\n')
+with open('results.txt', 'a') as handle :
+    handle.write(f'{args.station}, {np.mean(losses)}, {np.std(losses)}\n')
